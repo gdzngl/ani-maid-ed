fc3c1107f463c7588aca219de84f53f9f06ec225
Gretchen Dziengel <gdziengel@gmail.com>
2013-06-07 09:29:19 -0500

    moved mailto rules into daily
    Because I want to run them daily.

--- daily.rb
+++ daily.rb
@@ -4,4 +4,24 @@ Maid.rules do
     files = dir('~/Dropbox/_maid/_manual-sort/*')
     `echo #{files.inspect} | mail -s "Files to sort: #{files.length}" #{MY_EMAIL}`
   end
+
+  rule 'Send Files in mailto folders' do
+    emails = dir('~/Dropbox/_maid/mailto/*')
+    for email in emails
+      address = File.basename(email)
+
+      dir("~/Dropbox/_maid/mailto/#{address}/*").each do |file|
+        log(address)
+        fn = File.basename(file)
+        mod_time = modified_at(file)
+        log("uuencode #{file} #{fn} | mail -s '#{fn}' #{address}")
+        cmd("uuencode #{file} #{fn} | mail -s '#{fn}' #{address} && touch #{file}")
+        new_mod_time = modified_at(file)
+        unless mod_time == new_mod_time
+          move(file, '~/Dropbox/_maid/processed')
+        end
+      end
+    end
+  end
+
 end
--- mailto_rules.rb
+++ mailto_rules.rb
@@ -1,24 +1,2 @@
 Maid.rules do
-  emails = dir('~/Dropbox/_maid/mailto/*')
-  rule 'Send Files in mailto folders' do
-    for email in emails
-      address = File.basename(email)
-
-      dir("~/Dropbox/_maid/mailto/#{address}/*").each do |file|
-        log(address)
-        fn = File.basename(file)
-        mod_time = modified_at(file)
-        log("uuencode #{file} #{fn} | mail -s '#{fn}' #{address}")
-        cmd("uuencode #{file} #{fn} | mail -s '#{fn}' #{address} && touch #{file}")
-        new_mod_time = modified_at(file)
-        unless mod_time == new_mod_time
-          move(file, '~/Dropbox/_maid/processed')
-        else
-          puts mod_time.to_s
-          puts new_mod_time.to_s
-        end
-
-      end
-    end
-  end
 end
