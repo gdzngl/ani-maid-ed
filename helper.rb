99d0360dae5bbbf13db4681fcac42271b8c370ca
Gretchen Dziengel <gdziengel@gmail.com>
2013-06-07 09:34:29 -0500

    rewrote the date matching
    The new way deals with extra spaces that OCR can add.

--- helper.rb
+++ helper.rb
@@ -5,27 +5,32 @@ def find_date(text, date_pre, date_format)
   log("-----Date Prefix: #{date_pre.source}")
   log("-----Date Format: #{date_format}")
   date_only = ""
-  spaces = true
   case date_format
   when "%m/%d/%Y", "%m/%d/%y"
-    date_only = /(\d?\d\/\s?\d?\d\/\s?\d?\d?\d\d)/
-    spaces = false
+    date_only = /(\d?\d)\/\s?(\d?\d)\/\s?(\d?\d?\d\d)/
   when "%b. %d, %Y"
-    date_only = /([a-z][a-z][a-z]\.\s\d?\d\,\s\d\d\d\d)/
+    date_only = /([a-z][a-z][a-z])\.?\s(\d?\d)\,\s(\d\d\d\d)/
   when "%B %d, %Y"
-    date_only = /(\w*\s\d?\d\,\s\d\d\d\d)/
+    date_only = /(\w*)\s(\d?\d)\,\s(\d\d\d\d)/
+  when "%m-%d-%y"
+    date_only = /(\d?\d)\-(\d?\d)\-(\d?\d?\d\d)/
   end
   date_regex = /#{date_pre.source}#{date_only.source}/
   log("-----Date Regex: #{date_regex.source}")
   text_date = text.match(date_regex)
   text_date ||= [nil, nil, nil]
   log("-----Date Match: #{text_date.inspect}")
+  case date_format
+  when "%m/%d/%Y", "%m/%d/%y"
+    this_date = "#{text_date[2]}/#{text_date[3]}/#{text_date[4]}"  
+  when "%b. %d, %Y"
+    this_date = "#{text_date[2]}. #{text_date[3]}, #{text_date[4]}"  
+  when "%B %d, %Y"
+    this_date = "#{text_date[2]} #{text_date[3]}, #{text_date[4]}"  
+  when "%m-%d-%y"
+    this_date = "#{text_date[2]}-#{text_date[3]}-#{text_date[4]}"  
+  end
   unless text_date[2].nil?
-    if date_format.include?(' ')
-      this_date = text_date[2]
-    else
-      this_date = text_date[2].gsub(/\s/, '')
-    end
     real_date = Date.strptime(this_date, date_format)
     return real_date.strftime("%F")
   else
